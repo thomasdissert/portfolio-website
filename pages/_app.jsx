d7eb2ec3d3e68e000ea232eea12a67e7843a29f0
Thomas Dissert <thomas.dissert@abusix.com>
2020-06-13 19:02:35 +0200

    added tailwind css

---
+++ pages/_app.jsx
@@ -0,0 +1,5 @@
+import "../styles/index.css";
+
+export default ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,34 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">Thomas Dissert</h1>
-        <h1 className="subtitle">Full Stack Developer</h1>
+      <main className="width-900 text-right font-mono">
+        <div className="text-5xl font-extrabold">Thomas Dissert</div>
+        <div className="text-2xl text-purple-800 uppercase mb-6">
+          Full Stack Developer
+        </div>
 
-        <p className="description">
+        <p className="text-2xl leading-normal mt-3 font-mono">
           Making the internet a safer place by creating a network security
-          platform at <a href="https://abusix.com">abusix, Inc.</a> using a
-          state of the art stack including <a>React.js</a>, <a>Next.js</a>,{" "}
-          <a>Gatsby.js</a>, <a>GraphQL</a>
+          platform at{" "}
+          <a className="text-purple-800" href="https://abusix.com">
+            abusix
+          </a>{" "}
+          using a state of the art stack including{" "}
+          <span className="border bg-gray-200 rounded shadow-xs px-1">
+            React.js
+          </span>
+          ,{" "}
+          <span className="border bg-gray-200 rounded shadow-xs px-1">
+            Next.js
+          </span>
+          ,{" "}
+          <span className="border bg-gray-200 rounded shadow-xs px-1">
+            Gatsby.js
+          </span>
+          ,{" "}
+          <span className="border bg-gray-200 rounded shadow-xs px-1">
+            GraphQL
+          </span>
         </p>
 
         <div className="grid">
@@ -51,150 +70,6 @@ export default function Home() {
       </main>
 
       <footer>© 2020 Thomas Dissert.</footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          max-width: 960px;
-          margin: auto;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: right;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          opacity: 0.5;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-          color: #201f33;
-        }
-
-        .subtitle {
-          color: #745c97;
-        }
-
-        .title,
-        .subtitle,
-        .description {
-          text-align: end;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-
-          background-color: #f2f6f8;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
