3715b92cecf7152460556e9c25e1a828c8c1ce5f
Thomas Dissert <thomas.dissert@abusix.com>
2020-06-13 15:12:08 +0200

    added personal portfolio slogan

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,26 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Thomas Dissert</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className="title">Thomas Dissert</h1>
+        <h1 className="subtitle">Full Stack Developer</h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Making the internet a safer place by creating a network security
+          platform at <a href="https://abusix.com">abusix, Inc.</a> using a
+          state of the art stack including <a>React.js</a>, <a>Next.js</a>,{" "}
+          <a>Gatsby.js</a>, <a>GraphQL</a>
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
+          {/* <a href="https://nextjs.org/docs" className="card">
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
@@ -44,25 +46,17 @@ export default function Home() {
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
-          </a>
+          </a> */}
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+      <footer>© 2020 Thomas Dissert.</footer>
 
       <style jsx>{`
         .container {
           min-height: 100vh;
-          padding: 0 0.5rem;
+          max-width: 960px;
+          margin: auto;
           display: flex;
           flex-direction: column;
           justify-content: center;
@@ -70,12 +64,11 @@ export default function Home() {
         }
 
         main {
-          padding: 5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
           justify-content: center;
-          align-items: center;
+          align-items: right;
         }
 
         footer {
@@ -85,6 +78,7 @@ export default function Home() {
           display: flex;
           justify-content: center;
           align-items: center;
+          opacity: 0.5;
         }
 
         footer img {
@@ -102,26 +96,21 @@ export default function Home() {
           text-decoration: none;
         }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
         .title {
           margin: 0;
           line-height: 1.15;
           font-size: 4rem;
+          color: #201f33;
+        }
+
+        .subtitle {
+          color: #745c97;
         }
 
         .title,
+        .subtitle,
         .description {
-          text-align: center;
+          text-align: end;
         }
 
         .description {
@@ -198,6 +187,8 @@ export default function Home() {
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
+
+          background-color: #f2f6f8;
         }
 
         * {
@@ -205,5 +196,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
